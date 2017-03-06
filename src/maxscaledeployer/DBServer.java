bc3ef5a74087f75d11eae286ea850b6ca0cd7a44
Oren Bissick <obissick@greathealthworks.com>
2017-03-06 10:57:07 -0500

    change DBServer class to inherit from Server class.

--- src/maxscaledeployer/DBServer.java
+++ src/maxscaledeployer/DBServer.java
@@ -9,46 +9,24 @@ package maxscaledeployer;
  *
  * @author obissick
  */
-public class DBServer {
-    private String hostname;
+public class DBServer extends Server{
+    
     private int port;
-    private String username;
-    private String password;
     
     DBServer(){
         
     }
     DBServer(String host, int port, String user, String password){
-        this.hostname = host;
+        super(host,user,password);     
         this.port = port;
-        this.username = user;
-        this.password = password;
     }
-    
-    public void setHost(String host){
-        this.hostname = host;
-    }
+
     public void setPort(int port){
         this.port = port;
     }
-    public void setUser(String user){
-        this.username = user;
-    }
-    public void setpassword(String password){
-        this.password = password;
-    }
-    
-    public String getHost(){
-        return this.hostname;
-    }
+
     public int getPort(){
         return this.port;
-    }
-    public String getUser(){
-        return this.username;
-    }
-    public String getPassword(){
-        return this.password;
     }
     
     @Override
